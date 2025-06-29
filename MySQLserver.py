04e420159387d26a035e04b15e085bcba285ed50
Divine Lubiku <Divinelubiku6@gmail.com>
2025-06-29 16:08:58 +0200

    Completed file error free

--- MySQLserver.py
+++ MySQLserver.py
@@ -1,1 +1,24 @@
-user="hi"
+import mysql.connector;
+from mysql.connector import Error
+
+def connection():
+    try:
+        mydb=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Dee99_Christelle#6",
+            database="alx_book_store"
+        )
+
+        if mydb.is_connected():
+            print(mydb.server_info)
+
+    except Error as e:
+        print(e)
+
+    finally:
+         if mydb.is_connected():
+            mydb.close() # type: ignore
+
+if __name__== "__main__":
+    connection()
