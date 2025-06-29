caad679e5f8def3181150d2e8c66f51713a5f423
Divine Lubiku <Divinelubiku6@gmail.com>
2025-06-29 19:22:01 +0200

    Change to address +1 whitespace

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 INSERT INTO customer(customer_id, customer_name,email,address)
 VALUES
 (2, 'Blessing Malik','bmalik@sandtech.com','124 Happiness  Ave.'),
-(3, 'Obed Ehoneah', 'obed@sandtech.com','125 Happiness Ave.'),
-(4, 'Nehemial Kamolu','nkamolu#sandtech.com','126 Hapiness Ave.');
+(3, 'Obed Ehoneah', 'obed@sandtech.com','125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu','nkamolu#sandtech.com','126 Hapiness  Ave.');
